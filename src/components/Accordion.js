3928a25eec0d8dccc2ebe9b9e82db9d6bedee022
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-11-28 23:22:08 +0200

    add handler

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (index, item) => {
+    console.log("Title Clicked", index);
+  };
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
