b9cf2a1306e710c35e72dfedcd8cc57d5ea71ed8
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-05 21:19:14 +0200

    display list on page

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,7 +22,7 @@ const Search = () => {
   }, [term]);
   const renderedResults = results.map((result) => {
     return (
-      <div className="item">
+      <div key={result.pageid} className="item">
         <div className="content">
           <div className="header">{result.title}</div>
           {result.snippet}
@@ -42,6 +42,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
