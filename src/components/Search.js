bf5a2176cae5506947456104bccb6ee9de23c2a6
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-06 13:39:47 +0200

    linking to wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("programming");
+  const [term, setTerm] = useState("breaking bad");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
@@ -23,6 +23,14 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui  button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
