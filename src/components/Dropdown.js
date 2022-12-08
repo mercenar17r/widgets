9ca19890aeb5b448c846cf4206e6840245ed4586
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-08 23:51:03 +0200

    map over options

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,14 @@
 import react from "react";
 
-
-const Dropdown=()=>{
-    return <h1>Dropdown</h1>
-
-}
-
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
+  return <h1>Dropdown</h1>;
+};
 
 export default Dropdown;
