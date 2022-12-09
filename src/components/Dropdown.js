ad29f6c36c1f5c396f2e6c036473fbe8c8086ea1
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-09 22:32:00 +0200

    hide and show menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -20,10 +20,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visibleactive" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu visible ${open ? "transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
