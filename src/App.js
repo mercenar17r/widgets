7e37220598e9f6d2de99fc77de7eaba9563bea5e
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-12-17 19:06:00 +0200

    add translate component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
+import Translate from "./components/Translate";
 
 const items = [
   {
