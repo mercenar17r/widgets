98b9d3b4c5e730cb99ecbfdb1eecb255f10d96a9
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-12 23:12:48 +0200

    add options

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+
+const options = [
+  {
+    label: "Africans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+];
 
 const Translate = () => {
-  return <div>Translate</div>;
+  return (
+    <div>
+      <Dropdown options={options} />
+    </div>
+  );
 };
 
 export default Translate;
