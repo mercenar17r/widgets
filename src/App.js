8461e1e98bc8d535e1647752d493c5519f72cf7b
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-14 23:20:48 +0200

    add pathname for each comp

--- src/App.js
+++ src/App.js
@@ -33,11 +33,30 @@ const options = [
     value: "blue",
   },
 ];
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
 
 export default () => {
-  return (
-    <div>
-      <Translate />
-    </div>
-  );
+  return <div>{Accordion()}</div>;
 };
