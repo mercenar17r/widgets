ebab6c7cea300dc0250d59e73ce5376c5ec128a2
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-15 23:28:29 +0200

    add handler to prevent page reload

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Link = () => {
-  return <a>Link</a>;
+const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
